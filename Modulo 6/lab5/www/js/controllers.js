41a0ed185f9f2c9358b9a9453058eb4d5fee4d72
jQuispe <jeqs.quispe@gmail.com>
2016-08-06 12:29:31 -0500

    repo update

--- Modulo 6/lab5/www/js/controllers.js
+++ Modulo 6/lab5/www/js/controllers.js
@@ -2,15 +2,38 @@ angular.module('app.controllers', ['ngCordova'])
   
 .controller('pageCtrl', ['$scope', '$stateParams', '$cordovaDeviceMotion', 
 	function ($scope, $stateParams, $cordovaDeviceMotion) {
-		$scope.getCurrentDeviceMotion = function(){ 
-			$cordovaDeviceMotion.getCurrentAcceleration().then(function(result) {
-				$scope.x = result.x;
-				$scope.y = result.y;
-				$scope.z = result.z;
-				$scope.timeStamp = result.timestamp;
-	    }, function(err) {
-	       $scope.timeStamp ="Sucedio un error";
-	    });
-	}
 
-}])
+	document.addEventListener("deviceready", function () {
+
+	    $cordovaDeviceMotion.getCurrentAcceleration().then(function(result) {
+	      $scope.x = result.x;
+	      $scope.y = result.y;
+	      $scope.z = result.z;
+	      $scope.timeStamp = result.timestamp;
+	    }, function(err) {
+	      $scope.timeStamp ="Sucedio un error";
+	    });
+
+	}, false); // fin deviceready
+
+
+    $scope.watchAcceleration = function () {
+   	var options = { frequency: 1000 };  
+   	
+  	$cordovaDeviceMotion.watchAcceleration(options).then(
+    	function() { },  
+    	function(err) {
+    		 $scope.timeStamp ="Sucedio un error";
+    	},
+		function(result) {
+	      	$scope.x = result.x;
+	      	$scope.y = result.y;
+	      	$scope.z = result.z;
+	      	$scope.timeStamp = result.timestamp;
+	     }
+     );
+   };
+
+	} // fin function function ($scope, $stateParams, $cordovaDeviceMotion) {
+
+])
--- Modulo 6/lab6/www/js/controllers.js
+++ Modulo 6/lab6/www/js/controllers.js
@@ -3,20 +3,24 @@ angular.module('app.controllers', ['ngCordova'])
 .controller('pageCtrl', ['$scope', '$stateParams', '$cordovaNetwork', 
 	function ($scope, $stateParams, $cordovaNetwork) {
 
-		document.addEventListener("deviceready", function () {
-			$scope.type = $cordovaNetwork.getNetwork();
-			$scope.isOnline = $cordovaNetwork.isOnline();
-			$scope.isOffline = $cordovaNetwork.isOffline();
+		$scope.updateState = function(){
 
-			// listen for Online event
-			$rootScope.$on('$cordovaNetwork:online', function(event, networkState){
-		      $scope.netState = networkState;
-		    })
+			document.addEventListener("deviceready", function () {
+				$scope.type = $cordovaNetwork.getNetwork();
+				$scope.isOnline = $cordovaNetwork.isOnline();
+				$scope.isOffline = $cordovaNetwork.isOffline();
 
-		    // listen for Offline event
-		    $rootScope.$on('$cordovaNetwork:offline', function(event, networkState){
-		      $scope.netState = networkState;
-		    })
-  		}, false);
+				// listen for Online event
+				$rootScope.$on('$cordovaNetwork:online', function(event, networkState){
+			      $scope.netState = networkState;
+			    })
+
+			    // listen for Offline event
+			    $rootScope.$on('$cordovaNetwork:offline', function(event, networkState){
+			      $scope.netState = networkState;
+			    })
+	  		}, false);
+
+  		}
 
 }])
