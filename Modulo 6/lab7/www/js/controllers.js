ee772a92a34a762ef63b6183542119dd1b5811ac
jQuispe <jeqs.quispe@gmail.com>
2016-08-11 17:38:26 -0500

    update repo

---
+++ Modulo 6/lab7/www/js/controllers.js
@@ -0,0 +1,26 @@
+angular.module('app.controllers', ['ngCordova'])
+  
+.controller('pageCtrl', ['$scope', '$stateParams', '$cordovaNetwork', 
+	function ($scope, $stateParams, $cordovaNetwork) {
+
+		$scope.updateState = function(){
+
+			document.addEventListener("deviceready", function () {
+				$scope.type = $cordovaNetwork.getNetwork();
+				$scope.isOnline = $cordovaNetwork.isOnline();
+				$scope.isOffline = $cordovaNetwork.isOffline();
+
+				// listen for Online event
+				$rootScope.$on('$cordovaNetwork:online', function(event, networkState){
+			      $scope.netState = networkState;
+			    })
+
+			    // listen for Offline event
+			    $rootScope.$on('$cordovaNetwork:offline', function(event, networkState){
+			      $scope.netState = networkState;
+			    })
+	  		}, false);
+
+  		}
+
+}])
